5bfcab28c41fd23fe6ba9f89d8e8290420a5d39b
midomato <midotomato@gmail.com>
2025-06-14 16:01:40 +0900

    dmain

--- posts/calendar.md
+++ posts/calendar.md
@@ -7,6 +7,6 @@ ___
 
 今日はこのサイト内でカレンダーを作りました。どこにも繋がっていないのでたどり着くまで超めんどい！
 
-<span style="color: purple">[カレンダーはこちら](https://my-blog-8wj4.vercel.app/calendar)</span>からクリックして飛べます。
+<span style="color: purple">[カレンダーはこちら](./calendar)</span>からクリックして飛べます。
 
 まだ、最低限の機能しかないので徐々に増やせて行けたらなと思います。
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,40 @@
-'use client'
+import Link from 'next/link'
+import { getAllPosts } from '@/lib/posts'
 
-import { useSession, signIn, signOut } from "next-auth/react"
-
-export default function Home() {
-  const { data: session } = useSession()
-
-  if (!session) {
-    return (
-      <div style={{ padding: "2rem" }}>
-        <h1>ログインしていません</h1>
-        <button onClick={() => signIn("google")}>Googleでログイン</button>
-      </div>
-    )
-  }
+export default async function PostListPage() {
+  const posts = await getAllPosts()
 
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>こんにちは、{session.user?.name} さん！</h1>
-      <p>あなたのメールアドレスは {session.user?.email}</p>
-      <button onClick={() => signOut()}>ログアウト</button>
-    </div>
+    <main className="post-list-page max-w-3xl mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6"><span style={{color: 'green'}}>Midot.</span>技術ブログ一覧</h1>
+      <ul className="space-y-4">
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link href={`/posts/${post.slug}`}>
+              <h2 className="text-xl font-semibold">{post.title}</h2>
+              <p className="text-sm text-gray-500">{post.date}</p>
+            </Link>
+            <div className="flex gap-10 text-sm text-gray-400 mt-0.5">
+              {post.tag?.map((tag: string) => (
+                <Link href={`/tags/${tag}`} key={tag}>
+                <span
+                  style={{
+                  marginRight: '8px',
+                  padding: '4px 8px',
+                  backgroundColor: '#2f4f4f',
+                  borderRadius: '8px',
+                  fontSize: '0.875rem',
+                  color: '#eee',
+                  }}
+                  >
+                    {tag}
+                  </span>
+                  </Link>
+              ))}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </main>
   )
 }
